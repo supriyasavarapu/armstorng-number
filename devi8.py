8134a336289bc3b909c81597c707a53e8dade3f1
supriyasavarapu <146428159+supriyasavarapu@users.noreply.github.com>
2025-07-08 21:12:08 +0700

    Add files via upload

---
+++ devi8.py
@@ -0,0 +1,11 @@
+
+# n=input()
+# num=str(n)
+# digits=len(num)
+# sum=0
+# for i in num:
+#   sum+=int(i)**digits
+# if (sum == int(n)):                              #0/p=153
+#     print("armstrong")                            #   armstrong
+# else:
+#     print("not arrmstrong")
